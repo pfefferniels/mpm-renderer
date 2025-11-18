7cb36a1ae71ecd9eacb7eeb2f744a0780e20ea80
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-11-18 22:30:48 +0100

    more sophisticated ways of isolation

--- core/src/main/java/meicotools/core/Isolation.java
+++ core/src/main/java/meicotools/core/Isolation.java
@@ -2,11 +2,14 @@ package meicotools.core;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import meico.mei.Mei;
 import meico.mpm.Mpm;
+import meico.mpm.elements.Dated;
 import meico.mpm.elements.Performance;
 import meico.mpm.elements.maps.DynamicsMap;
 import meico.mpm.elements.maps.MetricalAccentuationMap;
@@ -19,9 +22,12 @@ import meico.mpm.elements.maps.data.MovementData;
 import meico.mpm.elements.maps.data.RubatoData;
 import meico.mpm.elements.maps.data.TempoData;
 import meico.msm.Msm;
+import net.sf.saxon.expr.Component.M;
 import nu.xom.Attribute;
 import nu.xom.Element;
+import nu.xom.Node;
 import nu.xom.Nodes;
+import nu.xom.XPathContext;
 
 public class Isolation {
     public static double[] isolateInstructions(
@@ -192,33 +198,193 @@ public class Isolation {
         };
     }
 
-    private static final double MIN_RANGE_DISTANCE = 3600;
-    private static final double SUBRANGE_SIZE = 2880;
-    private static final double CONTEXT_SIZE = 1440;
-    
-    public static double[] pick(double[] range) {
-        double min = range[0];
-        double max = range[1];
-        
-        // Ensure the given range has minimum distance of 1440
-        if (max - min < MIN_RANGE_DISTANCE) {
-            System.out.println("Given range is too small. Expanding to minimum distance of " + MIN_RANGE_DISTANCE);
-            return range;
-        } 
-        
-        // Calculate maximum start position to ensure we can fit a 720 range
-        double maxStart = max - SUBRANGE_SIZE;
-        
-        // Pick random start within valid bounds
-        double start = min + Math.random() * (maxStart - min);
-        double end = start + SUBRANGE_SIZE;
-        
-        return new double[]{start, end};
+    public static double[] pickExample(Performance performance, Set<String> keepIds) {
+        ArrayList<Double> relevantDates = new ArrayList<Double>();
+        Nodes nodes = performance.getXml().query("descendant::*[@date]");
+        if (nodes.size() == 0) {
+            System.out.println("No dated elements in performance");
+            return new double[] { 0.0, 0.0 };
+        }
+
+        for (Node node : nodes) {
+            Element el = (Element) node;
+            String dateStr = el.getAttributeValue("date");
+            String xmlId = el.getAttributeValue("id", "http://www.w3.org/XML/1998/namespace");
+            if (xmlId == null || !keepIds.contains(xmlId)) {
+                continue;
+            }
+
+            try {
+                Double d = Double.parseDouble(dateStr);
+                relevantDates.add(d);
+            } catch (Exception ignore) {}
+        }
+
+        if (relevantDates.isEmpty()) {
+            System.out.println("This should not happen: no relevant dates found for the provided MPM IDs.");
+            return new double[] {0.0, 0.0};
+        }
+
+        Collections.sort(relevantDates);
+
+        if (relevantDates.size() == 1) {
+            double date = relevantDates.get(0);
+            return new double[] {date, date + 1};
+        }
+
+        double lastDate = relevantDates.get(relevantDates.size() - 1);
+        double firstDate = relevantDates.get(0);
+        if ((lastDate - firstDate) <= 2880) {
+            return new double[] { firstDate, lastDate };
+        }
+
+        double distance = lastDate - firstDate;
+
+        return new double[] { firstDate, firstDate + distance / 3.0 };
+
+        /*
+
+        // Find the best cluster with at least 2 dates (smallest range)
+        double bestStart = relevantDates.get(0);
+        double bestEnd = relevantDates.get(1);
+        double bestRange = bestEnd - bestStart;
+
+        for (int i = 0; i < relevantDates.size() - 1; i++) {
+            for (int j = i + 1; j < relevantDates.size(); j++) {
+                double currentRange = relevantDates.get(j) - relevantDates.get(i);
+                if (currentRange < bestRange) {
+                    bestRange = currentRange;
+                    bestStart = relevantDates.get(i);
+                    bestEnd = relevantDates.get(j);
+                }
+            }
+        }
+
+        return new double[] { bestStart, bestEnd };*/
     }
 
-    public static double[] contextualize(double[] range) {
-        double start = Math.max(range[0] - CONTEXT_SIZE, 0);
-        double end = range[1] + CONTEXT_SIZE;
+    public static double[] contextualize(double[] range, double context, Performance perf) {
+        double contextInTicks = context * 4 * 720;
+        double start = Math.max(range[0] - contextInTicks, 0);
+        double end = range[1] + (contextInTicks / 2.0);
+
+        // Make sure, that the bits around should be played somewhat quieter.
+        Dated d = perf.getGlobal().getDated();
+        DynamicsMap map = (DynamicsMap) d.getMap(Mpm.DYNAMICS_MAP);
+
+        DynamicsData startData = map.getDynamicsDataAt(range[0]);
+        double startTarget = startData.getDynamicsAt(range[0]);
+
+        DynamicsData endData = map.getDynamicsDataAt(range[1]);
+        double endTarget = endData.getDynamicsAt(range[1]);
+
+        if (map == null) {
+            d.addMap(DynamicsMap.createDynamicsMap());
+        }
+        map.addDynamics(start, "30.0", Double.toString(startTarget), 0.4, 0.0);
+
+        for (int i = 0; i < map.size(); i++) {
+            DynamicsData dd = map.getDynamicsDataOf(i);
+            if (dd.startDate >= range[1] && dd.startDate <= endTarget) {
+                map.removeElement(i);
+                i--;
+            }
+        }
+
+        map.addDynamics(range[1], Double.toString(endTarget + 1), "30.0", 0.4, 0.0);
+        map.addDynamics(endTarget + 1, "30.0");
+
         return new double[]{ start, end };
+    }
+
+    public static double[] isolateMeasures(
+        Mei mei,
+        Msm msm,
+        Set<String> measures
+    ) {
+        boolean fromRepeat = false;
+        int fromMeasure = Integer.MAX_VALUE;
+        boolean toRepeat = false;
+        int toMeasure = 0;
+
+        for (String measure : measures) {
+            boolean withinRepeat = false;
+            if (measure.endsWith("-rpt")) {
+                measure = measure.substring(0, measure.length() - 4);
+                fromRepeat = true;
+            }
+            int m = Integer.parseInt(measure);
+            if (m < fromMeasure) {
+                fromMeasure = m;
+                fromRepeat = withinRepeat;
+            }
+            if (m > toMeasure) {
+                toMeasure = m;
+                toRepeat = withinRepeat;
+            }
+        }
+
+        fromMeasure += 1;
+        toMeasure += 1;
+
+        System.out.println("measures" + measures + " resolved to fromMeasure=" + fromMeasure + " " + toMeasure);
+
+        double[] fromDates = getDatesForMeasure(mei, msm, fromMeasure, fromRepeat);
+        double[] toDates = getDatesForMeasure(mei, msm, toMeasure, toRepeat);
+
+        System.out.println("Isolated measures " + fromMeasure + (fromRepeat ? "-rpt" : "") + " to " + toMeasure + (toRepeat ? "-rpt" : "") +
+            " corresponding to dates " + fromDates[0] + " to " + toDates[1]);
+
+        return new double[] { fromDates[0], toDates[1] };
+    }
+
+    private static double[] getDatesForMeasure(Mei mei, Msm msm, int measure, boolean inRepeat) {
+        XPathContext context = new XPathContext();
+        context.addNamespace("mei", "http://www.music-encoding.org/ns/mei");
+        context.addNamespace("xml", "http://www.w3.org/XML/1998/namespace");
+        Nodes nodes = mei.getRootElement().query("descendant::mei:measure[@n='" + measure + "']", context);
+        System.out.println("Found " + nodes.size() + " measure elements with n='" + measure + "' (inRepeat=" + inRepeat + ")");
+        if (nodes.size() == 0) {
+            throw new Error("No measure with n='" + measure + "' found in MEI.");
+        }
+
+        // Element nodes;
+        Element measureEl = (Element) nodes.get(0);
+        if (inRepeat && nodes.size() == 2) {
+            // pick the one which is in the repeat
+            measureEl = (Element) nodes.get(1);
+        }
+
+        Nodes notes = measureEl.query("descendant::mei:note", context);
+        double minDate = Double.POSITIVE_INFINITY;
+        double maxDate = 0.0;
+        for (int i = 0; i < notes.size(); i++) {
+            Element note = (Element) notes.get(i);
+            String xmlId = note.getAttributeValue("id", "http://www.w3.org/XML/1998/namespace");
+            if (xmlId == null) continue;
+
+            System.out.println("Test:" + msm.getRootElement().query("descendant::*[@xml:id]", context).size());
+
+            Nodes msmNotes = msm.getRootElement().query("descendant::*[@xml:id='" + xmlId + "']", context);
+            if (msmNotes.size() == 0) {
+                continue;
+            }
+
+            String date = ((Element) msmNotes.get(0)).getAttributeValue("date");
+            System.out.println("Note xml:id='" + xmlId + "' has date='" + date + "'");
+            try {
+                double d = Double.parseDouble(date);
+                if (d < minDate) {
+                    minDate = d;
+                }
+                if (d > maxDate) {
+                    maxDate = d;
+                }
+            } catch (Exception ignore) {}
+        }
+
+        System.out.println("Measure " + measure + (inRepeat ? "-rpt" : "") + " has date range: " + minDate + " to " + maxDate);
+
+        return new double[] { minDate, maxDate };
     }
 }
